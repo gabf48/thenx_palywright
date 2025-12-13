5b10a64851a69dfaaa71948dadc9d0d4414b15de
Gabriel Filip <gabf48@gmail.com>
2025-12-13 18:16:35 +0200

    close browser

--- scripts/scrape-products.js
+++ scripts/scrape-products.js
@@ -4,23 +4,32 @@ import 'dotenv/config';
 
 (async () => {
   const browser = await chromium.launch({
-    headless: true,
+    headless: true,       
     args: ['--start-maximized']
   });
 
   const context = await browser.newContext({
-    viewport: null
+    viewport: { width: 1920, height: 1080 }  
   });
 
   const page = await context.newPage();
-
   const loginPage = new LoginPage(page);
 
-  await loginPage.open(process.env.URL_LOGIN);
+  try {
+    // 2️⃣ Open login page
+    await loginPage.open(process.env.URL_LOGIN);
 
-  await loginPage.login(
-    process.env.LOGIN_EMAIL,
-    process.env.LOGIN_PASSWORD
-  );
+    // 3️⃣ Login
+    await loginPage.login(
+      process.env.LOGIN_EMAIL,
+      process.env.LOGIN_PASSWORD
+    );
 
+
+  } catch (error) {
+    console.error('Error during scraping:', error);
+    process.exitCode = 1; 
+  } finally {
+    await browser.close();
+  }
 })();
